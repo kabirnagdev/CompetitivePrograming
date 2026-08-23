fabc7933b8673bd1d800f326c3ab75fafee27250
Kabir Nagdev <174309715+kabirnagdev@users.noreply.github.com>
2026-08-23 22:05:56 +0530

    [1004] [Time Beats: 32.79%] [Memory Beats: 36.93%] - LeetPush

---
+++ 1004-max-consecutive-ones-iii/1004-Max-Consecutive-Ones-III.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+int left = 0;
+        int max_length = 0;
+        int zeros_count = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            if (nums[right] == 0) {
+                zeros_count++;
+            }
+            while (zeros_count > k) {
+                if (nums[left] == 0) {
+                    zeros_count--;
+                }
+                left++;
+            }
+
+            max_length = std::max(max_length, right - left + 1);
+        }
+
+        return max_length;
+    }
+};
